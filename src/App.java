9211ce52c8ff11d736e9e96d24327fdb4404667e
Iker Ezcurra <ikdha@plaiaundi.net>
2024-11-07 10:53:02 +0100

    commit CommitIker

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hola, soy Gabriel");
+        System.out.println("Hola, soy Iker");
     }
 }
